60492cad7a3450e7af272685c9dd33c526382619
Bozhan Liang <lbz@rivensbane.com>
2025-08-17 17:50:03 +1200

    test message feature

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,34 @@
+import database from "@react-native-firebase/database";
 import React from "react";
-import { View } from "react-native";
-import TinderSwipePrototype from "./TinderSwipePrototype.jsx";
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+
+// Define interface for message type
+interface Message {
+    [key: string]: any;
+}
 
 export default function Index() {
+    const [messages, setMessages] = React.useState<Message[]>([]);
+    const [text, setText] = React.useState<string>("");
+
+    React.useEffect(() => {
+        const reference = database().ref("/chats/one/messages");
+
+        reference.on("child_added", (snapshot) => {
+            const message = snapshot.val();
+            setMessages((prevMessages) => [...prevMessages, message]);
+        });
+
+        // Clean up listener on unmount
+        return () => reference.off("child_added");
+    }, []);
+
+    const sendMessage = () => {
+        const reference = database().ref("/chats/one/messages");
+        reference.push({ message: text, sender: "Phone Test" });
+        setText("");
+    };
+
     return (
         <View
             style={{
@@ -11,7 +37,26 @@ export default function Index() {
                 alignItems: "center",
             }}
         >
-            <TinderSwipePrototype />
+            <ScrollView style={styles.container}>
+                <Text>{messages.map((msg) => msg.message).join("\n")}</Text>
+            </ScrollView>
+            <TextInput
+                style={styles.input}
+                placeholder="Type here to translate!"
+                onChangeText={(newText) => setText(newText)}
+                defaultValue={text}
+            />
+            <Button onPress={sendMessage} title="Send message" />
         </View>
     );
 }
+const styles = StyleSheet.create({
+    input: {
+        borderWidth: 1,
+        borderColor: "blue", // Sets the border color to blue
+        padding: 10,
+    },
+    container: {
+        flexGrow: 0,
+    },
+});
