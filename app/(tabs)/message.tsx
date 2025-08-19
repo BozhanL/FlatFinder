8cb43fb95e7dfac6c4d623c5b7a44becfc41c37f
G2CCC <167666603+G2CCC@users.noreply.github.com>
2025-08-19 22:52:32 +1200

    App shell

---
+++ app/(tabs)/message.tsx
@@ -0,0 +1,9 @@
+import HeaderLogo from "@/components/HeaderLogo";
+import { View } from "react-native";
+export default function MessageScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: "#fff" }}>
+      <HeaderLogo></HeaderLogo>
+    </View>
+  );
+}
---
+++ app/_layout.tsx
@@ -0,0 +1,28 @@
+import {
+  PlayfairDisplay_700Bold,
+  useFonts,
+} from "@expo-google-fonts/playfair-display";
+import { Stack } from "expo-router";
+import { View, ActivityIndicator } from "react-native";
+
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    PlayfairDisplay_700Bold,
+  });
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+  return (
+    <Stack>
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen
+        name="(modals)"
+        options={{ presentation: "modal", headerShown: false }}
+      />
+    </Stack>
+  );
+}
