1d123771a651a66bf418596ef5916dee84357bfa
Bozhan Liang <lbz@rivensbane.com>
2025-08-30 13:09:41 +1200

    Fix test

---
+++ src/__tests__/components/HeaderLogo.tsx
@@ -0,0 +1,11 @@
+import HeaderLogo from "@/components/HeaderLogo";
+import { render, screen } from "@testing-library/react-native";
+
+describe("<HeaderLogo />", () => {
+  test("Text renders correctly on HeaderLogo", async () => {
+    render(<HeaderLogo />);
+    const elements = screen.getAllByText("FlatFinder");
+    expect(elements).toHaveLength(1);
+    expect(elements[0]).toBeOnTheScreen();
+  });
+});
