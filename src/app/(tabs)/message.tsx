5d6a9216a811844d20e0d4170ca86fdc38c5221b
Bozhan Liang <lbz@rivensbane.com>
2025-09-09 17:25:52 +1200

    Move login and create group buttons to profile screen

--- src/app/(tabs)/message.tsx
+++ src/app/(tabs)/message.tsx
@@ -1,22 +1,12 @@
 import HeaderLogo from "@/components/HeaderLogo";
 import MessageList from "@/components/message/MessageList";
-import { createGroup } from "@/services/message";
 import {
   FirebaseAuthTypes,
   getAuth,
   onAuthStateChanged,
-  signInWithEmailAndPassword,
-  signOut,
 } from "@react-native-firebase/auth";
 import { useEffect, useState } from "react";
-import {
-  ActivityIndicator,
-  Alert,
-  Button,
-  Text,
-  TextInput,
-  View,
-} from "react-native";
+import { ActivityIndicator, View } from "react-native";
 
 export default function MessageView() {
   return (
@@ -32,7 +22,6 @@ function Message() {
   const [user, setUser] = useState<FirebaseAuthTypes.User | null | undefined>(
     undefined,
   );
-  const [groupMembers, setGroupMembers] = useState<string>("");
 
   useEffect(() => {
     const subscriber = onAuthStateChanged(getAuth(), (user) => {
@@ -41,62 +30,13 @@ function Message() {
     return subscriber; // unsubscribe on unmount
   }, []);
 
-  if (user === undefined) {
+  if (!user) {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <ActivityIndicator />
       </View>
     );
-  } else if (!user) {
-    return <Login />;
   }
 
-  async function handleCreateGroup() {
-    const groupId = await createGroup(groupMembers.split(","));
-    if (groupId) {
-      Alert.alert("Group created!", `Group ID: ${groupId}`);
-    }
-  }
-
-  return (
-    <View>
-      <Button title="Logout" onPress={handleLogout} />
-      <TextInput value={groupMembers} onChangeText={setGroupMembers} />
-      <Button title="Create Group" onPress={handleCreateGroup} />
-      <MessageList uid={user.uid} />
-    </View>
-  );
-}
-
-// Removed old getGroupName helper; logic consolidated inside MessageList effects.
-
-function Login() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  return (
-    <View>
-      <Text>Login</Text>
-      <TextInput
-        placeholder="Email"
-        value={email}
-        onChangeText={(t) => setEmail(t)}
-      />
-      <TextInput
-        placeholder="Password"
-        value={password}
-        onChangeText={(newText) => setPassword(newText)}
-        secureTextEntry
-      />
-      <Button title="Login" onPress={() => handleLogin(email, password)} />
-    </View>
-  );
-}
-
-async function handleLogin(email: string, password: string) {
-  await signInWithEmailAndPassword(getAuth(), email, password);
-}
-
-async function handleLogout() {
-  await signOut(getAuth());
+  return <MessageList uid={user.uid} />;
 }
--- src/app/(tabs)/profile.tsx
+++ src/app/(tabs)/profile.tsx
@@ -1,10 +1,90 @@
 import HeaderLogo from "@/components/HeaderLogo";
-import { View } from "react-native";
+import { createGroup } from "@/services/message";
+import {
+  FirebaseAuthTypes,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+} from "@react-native-firebase/auth";
+import { useEffect, useState } from "react";
+import {
+  ActivityIndicator,
+  Alert,
+  Button,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 
 export default function ProfileScreen() {
+  // Set an initializing state whilst Firebase connects
+  const [user, setUser] = useState<FirebaseAuthTypes.User | null | undefined>(
+    undefined,
+  );
+  const [groupMembers, setGroupMembers] = useState<string>("");
+
+  useEffect(() => {
+    const subscriber = onAuthStateChanged(getAuth(), (user) => {
+      setUser(user);
+    });
+    return subscriber; // unsubscribe on unmount
+  }, []);
+
+  if (user === undefined) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator />
+      </View>
+    );
+  } else if (!user) {
+    return <Login />;
+  }
+
+  async function handleCreateGroup() {
+    const groupId = await createGroup(groupMembers.split(","));
+    if (groupId) {
+      Alert.alert("Group created!", `Group ID: ${groupId}`);
+    }
+  }
+
   return (
     <View style={{ flex: 1, backgroundColor: "#fff" }}>
-      <HeaderLogo></HeaderLogo>
+      <HeaderLogo />
+      <Button title="Logout" onPress={handleLogout} />
+      <TextInput value={groupMembers} onChangeText={setGroupMembers} />
+      <Button title="Create Group" onPress={handleCreateGroup} />
     </View>
   );
+}
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <View>
+      <Text>Login</Text>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={(t) => setEmail(t)}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={(newText) => setPassword(newText)}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={() => handleLogin(email, password)} />
+    </View>
+  );
+}
+
+async function handleLogin(email: string, password: string) {
+  await signInWithEmailAndPassword(getAuth(), email, password);
+}
+
+async function handleLogout() {
+  await signOut(getAuth());
 }
