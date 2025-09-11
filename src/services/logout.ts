60d2b98776a44c04342eafc372155ef368b6050a
Bozhan Liang <lbz@rivensbane.com>
2025-09-12 11:40:21 +1200

    Fix import error

--- src/services/logout.ts
+++ src/services/logout.ts
@@ -1,5 +1,5 @@
-import { deregisterToken } from "@/hooks/useNotification";
 import { getAuth, signOut } from "@react-native-firebase/auth";
+import { deregisterToken } from "./notification";
 
 export async function logout() {
   await deregisterToken();
