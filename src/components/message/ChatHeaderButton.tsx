ed7460f9a16848c28ef7d9047f7f6a3eba3290e8
Bozhan Liang <lbz@rivensbane.com>
2025-10-03 20:00:12 +1300

    use react-navigation-header-buttons for src\components\message\ChatHeaderButton.tsx

--- src/components/message/ChatHeaderButton.tsx
+++ src/components/message/ChatHeaderButton.tsx
@@ -1,9 +1,9 @@
 import { deleteGroup } from "@/services/message";
 import { blockUser } from "@/services/swipe";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 import { router } from "expo-router";
-import { useState, type JSX } from "react";
-import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
+import type { JSX } from "react";
+import { HiddenItem, OverflowMenu } from "react-navigation-header-buttons";
 
 export default function ChatHeaderButton({
   gid,
@@ -12,70 +12,22 @@ export default function ChatHeaderButton({
   gid: string;
   uid: string;
 }): JSX.Element {
-  const [showList, setShowList] = useState(false);
-  const insets = useSafeAreaInsets();
-
   return (
-    <View>
-      <TouchableOpacity
+    <OverflowMenu
+      OverflowIcon={({ color }) => (
+        <MaterialIcons name="more-horiz" size={23} color={color} />
+      )}
+      testID={"show-chat-header-button"}
+    >
+      <HiddenItem
+        title="Block"
         onPress={() => {
-          setShowList((prev) => !prev);
+          router.back();
+          void blockUser(gid, uid);
+          void deleteGroup(gid);
         }}
-        testID="show-chat-header-button"
-      >
-        <Text style={styles.buttonText}>...</Text>
-      </TouchableOpacity>
-
-      <Modal
-        animationType="none"
-        transparent={true}
-        visible={showList}
-        onRequestClose={() => {
-          setShowList(false);
-        }}
-      >
-        <TouchableOpacity
-          style={[styles.modalOverlay, { paddingTop: insets.top + 55 }]}
-          onPress={() => {
-            setShowList(false);
-          }}
-        >
-          <View style={styles.dropdownContainer}>
-            <TouchableOpacity
-              onPress={() => {
-                setShowList(false);
-                router.back();
-                void blockUser(gid, uid);
-                void deleteGroup(gid);
-              }}
-            >
-              <Text style={styles.dropdownItem}>Block</Text>
-            </TouchableOpacity>
-          </View>
-        </TouchableOpacity>
-      </Modal>
-    </View>
+        testID={"block-user-button"}
+      />
+    </OverflowMenu>
   );
 }
-
-const styles = StyleSheet.create({
-  buttonText: {
-    fontSize: 32,
-  },
-  modalOverlay: {
-    flex: 1,
-    justifyContent: "flex-start",
-    alignItems: "flex-end",
-    paddingRight: 10,
-  },
-  dropdownContainer: {
-    backgroundColor: "#DADADA",
-    borderRadius: 5,
-    padding: 10,
-  },
-  dropdownItem: {
-    paddingVertical: 8,
-    paddingHorizontal: 15,
-    fontSize: 16,
-  },
-});
