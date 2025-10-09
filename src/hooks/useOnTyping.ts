dbb37ceb8cbfb967a44577fc84db9a1ec475b6cc
Bozhan Liang <lbz@rivensbane.com>
2025-10-09 16:47:47 +1300

    Use lodash to debounce useOnTyping

--- src/hooks/useOnTyping.ts
+++ src/hooks/useOnTyping.ts
@@ -2,36 +2,61 @@
 // This file mainly contains code for IO, and unable to be tested in unit tests.
 import { TYPING_TIMER_LENGTH } from "@/types/consts";
 import { getDatabase, ref, remove, set } from "@react-native-firebase/database";
-import { useCallback, useState } from "react";
+import { useFocusEffect } from "expo-router";
+import { debounce } from "lodash";
+import { useCallback, useMemo, useRef } from "react";
 
-export default function useOnTyping(gid: string, uid: string): () => void {
-  const [timeoutVar, setTimeoutVar] = useState<number | null>(null);
+export default function useOnTyping(
+  gid: string,
+  uid: string,
+): (text: string) => void {
+  const isTyping = useRef(false);
+  const debouncedStopTyping = useMemo(
+    () =>
+      debounce(() => {
+        void remove(ref(getDatabase(), `typing/${gid}/${uid}`));
+        isTyping.current = false;
+        console.debug("stopped typing");
+      }, TYPING_TIMER_LENGTH),
+    [gid, uid],
+  );
 
-  const onTyping = useCallback(() => {
-    async function f(): Promise<void> {
-      try {
-        const db = getDatabase();
-        const typeRef = ref(db, `typing/${gid}/${uid}`);
-        if (timeoutVar) {
-          clearTimeout(timeoutVar);
-        } else {
-          await set(typeRef, true);
+  const onTyping = useCallback(
+    (text: string): void => {
+      async function f(): Promise<void> {
+        // Only set typing when there is text
+        if (text.length === 0) {
+          return;
         }
 
-        const timeoutId = setTimeout(() => {
-          void remove(typeRef).then(() => {
-            setTimeoutVar(null);
-          });
-        }, TYPING_TIMER_LENGTH);
+        console.debug("onTyping called", text);
 
-        setTimeoutVar(timeoutId);
-      } catch (e) {
-        console.error(e);
+        try {
+          if (!isTyping.current) {
+            const typeRef = ref(getDatabase(), `typing/${gid}/${uid}`);
+            await set(typeRef, true);
+            isTyping.current = true;
+          }
+
+          debouncedStopTyping();
+        } catch (e) {
+          console.error(e);
+        }
       }
-    }
 
-    return (): void => void f();
-  }, [gid, uid, timeoutVar]);
+      void f();
+    },
+    [debouncedStopTyping, gid, uid],
+  );
+
+  useFocusEffect(
+    useCallback(() => {
+      return (): void => {
+        debouncedStopTyping.flush();
+        console.debug("cleanup on unmount or blur");
+      };
+    }, [debouncedStopTyping]),
+  );
 
   return onTyping;
 }
