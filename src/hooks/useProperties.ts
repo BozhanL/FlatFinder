750f812d3dcb2f94dd0b833a6a08dac3e41b4be3
Anthony-8114 <jkj8114@autuni.ac.nz>
2025-10-10 20:26:33 +1300

    Update src/hooks/useProperties.ts
    Co-authored-by: James <lbz@rivensbane.com>

--- src/hooks/useProperties.ts
+++ src/hooks/useProperties.ts
@@ -81,7 +81,7 @@ export default function useProperties(): UsePropertiesResult {
         setLoading(false);
       },
     );
-  }, [user, authChecked]);
+  }, [user]);
 
   return { properties, loading, error };
 }
