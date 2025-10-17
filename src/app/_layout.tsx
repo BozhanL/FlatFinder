28ba403b5b255ec113baef1a53fe129ff059478e
Bozhan Liang <lbz@rivensbane.com>
2025-10-17 16:01:04 +1300

    use SplashScreen to hide loading for auth

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -2,17 +2,29 @@ import useInitialNotification from "@/hooks/useInitialNotification";
 import useMessageToken from "@/hooks/useMessageToken";
 import useNotification from "@/hooks/useNotification";
 import useUser from "@/hooks/useUser";
-import { Stack } from "expo-router";
-import type { JSX } from "react";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect, type JSX } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { HeaderButtonsProvider } from "react-navigation-header-buttons/HeaderButtonsProvider";
 
-export default function RootLayout(): JSX.Element {
+void SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout(): JSX.Element | null {
   const user = useUser();
   useNotification();
   useMessageToken();
   useInitialNotification();
+
+  useEffect(() => {
+    if (user !== undefined) {
+      SplashScreen.hide();
+    }
+  }, [user]);
+
+  if (user === undefined) {
+    return null;
+  }
 
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
