377dc5dba83329f29bb786945b03961ae808de4a
Anthony-8114 <jkj8114@autuni.ac.nz>
2025-10-22 21:02:29 +1300

    Update src/hooks/usePostForm.ts
    Co-authored-by: James <lbz@rivensbane.com>

--- src/hooks/usePostForm.ts
+++ src/hooks/usePostForm.ts
@@ -187,6 +187,9 @@ export default function usePropertyForm(): UsePropertyFormReturn {
         } else if (!Number.isInteger(contractNum)) {
           newErrors["minContractLength"] =
             "Contract length must be whole weeks";
+        } else if (contractNum < 0) {
+          newErrors["minContractLength"] =
+            "Contract length must be positive number";
         } else if (contractNum > 520) {
           newErrors["minContractLength"] = "Maximum 520 weeks (10 years)";
         }
