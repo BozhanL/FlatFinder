23ad179060dd408f0f3b77337982f5f8a4d3a039
Bozhan Liang <lbz@rivensbane.com>
2025-10-23 13:43:37 +1300

    Fix dockerfile

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -16,5 +16,5 @@ npm run prebuild:android:clean
 APK_PATH=$(find ./android/app/build/outputs/apk/release -name "*-release.apk" | head -n 1)
 
 zipalign -c -v 4 "$APK_PATH"
-apksigner sign --ks ${KEYSTORE_PATH} --ks-pass env:KEYSTORE_PASSWORD --out /output/app-release-signed.apk "$APK_PATH"
+apksigner sign --ks "${KEYSTORE_PATH}" --ks-pass env:KEYSTORE_PASSWORD --out /output/app-release-signed.apk "$APK_PATH"
 apksigner verify /output/app-release-signed.apk
